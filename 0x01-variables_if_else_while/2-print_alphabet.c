434a01f367f6b878ef20cfb75b1fc72367b41831
root <mary.serna.8903@gmail.com>
2019-06-20 21:11:51 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+\**
+*
+*main - Entry point
+*
+*Return: Always 0(Success)
+*\
+int main(avoid)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchart(ch);
+}
+putchart('\n');
+return(0);
+}
+
