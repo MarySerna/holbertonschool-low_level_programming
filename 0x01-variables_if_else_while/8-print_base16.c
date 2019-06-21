f4197ba16a756368595977cb90986349b999d653
root <mary.serna.8903@gmail.com>
2019-06-21 02:59:18 +0000

    Print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -17,19 +17,4 @@ for (h = 'a'; h <= 'f'; h++)
 }
 putchar('\n');
 return (0);
-int main(void)
-{
-	int a;
-
-	for (a = 0; a < 10; a++)
-	{
-		putchar(a + '0');
-	}
-	for (a = 'a'; a <= 'f'; a++)
-	{
-		putchar(a);
-	}
-	putchar('\n');
-
-	return (0);
 }
