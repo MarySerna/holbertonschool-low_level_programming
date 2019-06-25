6d890b00ebce1b351275ce842b59e29ff751ed86
maryserar8903 <mary.serna.8903@gmail.com>
2019-06-25 19:56:36 +0000

    fixing header

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,8 @@
 */
 int print_last_digit(int c)
 {
-if (c > 0 || c == 0)
-{
-_putchar(c % 10 + '0');
-return (c % 10);
-}
-else
-{
+if (c < 0)
 c = c * -1;
-_putchar(c % 10 + '0');
+_putchar((c % 10) + '0');
 return (c % 10);
-}
 }
