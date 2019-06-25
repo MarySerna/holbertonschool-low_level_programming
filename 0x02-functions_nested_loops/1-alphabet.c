e7cf5bae16fb7fd3d59976c7c2146202dfa11a26
maryserar8903 <mary.serna.8903@gmail.com>
2019-06-25 02:39:46 +0000

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* main - function to print
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
