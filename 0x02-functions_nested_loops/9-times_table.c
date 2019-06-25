75eaca8927ae33374d02e5d51fb3146568e15c7a
maryserar8903 <mary.serna.8903@gmail.com>
2019-06-25 05:30:15 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+* main - function to print
+*
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+int m, j, res;
+for (m = 0; m <= 9; m++)
+{
+for (j = 0; j <= 9; j++)
+{
+res = (m * j);
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && j != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
+}
