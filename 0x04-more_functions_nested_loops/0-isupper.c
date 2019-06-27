acbb70f18c8faaba37a8ed4ce0ccbc15d747a690
maryserar8903 <mary.serna.8903@gmail.com>
2019-06-27 18:34:56 +0000

    checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* _isuper - checks for uppercase character.
+* _isupper - checks for uppercase character.
 * @c: character.
 *
-* Return: 0.
+* Return: 1 if is uppercase 0 if not
 */
 int _isupper(int c)
 {
