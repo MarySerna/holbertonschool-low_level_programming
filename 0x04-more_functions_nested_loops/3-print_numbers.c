f0c62d7e6edb8e45574b7789600df228b13fccbc
maryserar8903 <mary.serna.8903@gmail.com>
2019-06-27 19:31:43 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_numbers - prints the numbes
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+char c = '0';
+while (c <= '9')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
