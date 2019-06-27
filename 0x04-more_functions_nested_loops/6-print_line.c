457c5bd60d5ef92a0fde2227322ea2efd15ca811
maryserar8903 <mary.serna.8903@gmail.com>
2019-06-27 21:02:30 +0000

    draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n :integer
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+int l = 0;
+
+if (n > 0)
+{
+while (l <= n)
+{
+_putchar('_');
+l++;
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
