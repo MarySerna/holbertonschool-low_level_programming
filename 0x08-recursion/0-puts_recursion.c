82ff61a5759cd4aff4bd7a00776d032f4b698c0a
maryserar8903 <mary.serna.8903@gmail.com>
2019-07-10 18:37:05 +0000

    Prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _puts_recursion - prints a string, followed by a new line
+* @s: character
+*
+*return: void
+*/
+void _puts_recursion(char *s)
+{
+unsigned int string  = 0;
+if (s[string]) =='\0';
+{
+_putchar('\n');
+return;
+}
+_putchar(s[string]);
+_puts_recursion(s +1);
+}
