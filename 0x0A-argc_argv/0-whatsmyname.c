2cef73b55307b09adb719eda7a6f7e7d0738ef7e
maryserar8903 <mary.serna.8903@gmail.com>
2019-07-13 21:15:44 +0000

    Its name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - program that prints its name.
+* @argc: argument that counts argument input
+* @argv: argument that stores the strings in an array of char* (strings)
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
