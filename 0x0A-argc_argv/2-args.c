9fb9c668727739922369cd1097da70f40b536eaf
maryserar8903 <mary.serna.8903@gmail.com>
2019-07-14 01:24:50 +0000

    a program that prints all arguments and a program that multiplies two numbers

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - a program that prints all arguments it receives
+* @argc: argument that counts argument input
+* @argv: argument the strings in an array of char*
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stlib.h>
+/**
+* main - a program that multiplies two numbers
+* @argc: argument that counts argument input
+* @argv: argument the strings in an array of char*
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2, mul;
+
+if (argc != 3)
+printf("Error\n");
+else
+{
+num_1 = atoi(argv[1]);
+num_2 = atoi(argv[2]);
+mul = num1 * num2;
+printf("%d\n", mul);
+}
+return (0);
+}
