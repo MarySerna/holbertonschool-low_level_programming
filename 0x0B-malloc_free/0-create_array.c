23187ef5297e8aa997d558d6da09e2964f1d48e0
maryserar8903 <mary.serna.8903@gmail.com>
2019-07-16 20:24:27 +0000

    function that creates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* create_array - function that creates an array of chars
+* @c: argument for characater
+* @size: for size if array
+*
+* Return: 0
+*/
+char *create_array(unsigned int size, char c)
+{
+char *array_1;
+unsigned int i = 0;
+
+array_1 = malloc(size * sizeof(c));
+while (i < size)
+{
+if (array_1 == NULL)
+{
+return (NULL);
+}
+array_1[i] = c;
+i++;
+}
+return (array_1);
+}
