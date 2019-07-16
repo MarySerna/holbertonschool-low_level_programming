1a657f13bd167a06be8888cb05a79f15197a147b
maryserar8903 <mary.serna.8903@gmail.com>
2019-07-16 23:41:15 +0000

    function that returns a pointer

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+
+/**
+* _strlen - check the code for Holberton School students.
+* @s: declaration of *s and paramters for the function _strlen
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int c;
+
+for (c = 0; s[c] != 0; c++)
+{
+}
+return (c);
+}
 /**
 * _strdup - function that returns a pointer
-* @str: argument for array
+* @str: parametro for array
 *
 * Return: 0
 */
 char *_strdup(char *str)
 {
-char *array_1;
-int i, leng;
-
-array_1 = malloc(sizeof(str));
-i = leng = 0;
-while (str[i] != '\0')
+char *s;
+int i;
+s = malloc((_strlen(str) + 1) * sizeof(char));
+for (i = 0; i < _strlen(str); i++)
 {
-leng++;
-i++;
+s[i] = str[i];
 }
-if (array_1 == NULL)
-return (NULL);
-i = 0;
-while (str[i] != '\0')
-{
-array_1[i] = str[i];
-i++;
-}
-return (array_1);
+s[i + 1] = '\0';
+return (s);
 }
