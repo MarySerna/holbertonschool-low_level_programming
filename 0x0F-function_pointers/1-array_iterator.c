dcdbd1fe00447202fd41dc117c43e82d9f60bfb9
maryserar8903 <mary.serna.8903@gmail.com>
2019-07-24 22:48:43 +0000

    thinking about a thing

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+* array_iterator - function given as a parameter on each element of an array
+* @array: parameter for array
+* @size:parameter for size
+* @action:parameter for action
+*
+* Return:nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+
+	for (a = 0; a < size; a++)
+	{
+	action(array[a]);
+	}
+}
