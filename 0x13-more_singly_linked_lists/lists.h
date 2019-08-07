7e772c5f338525eaf8029c7af0d7c9c19184889f
maryserar8903 <mary.serna.8903@gmail.com>
2019-08-07 21:35:39 +0000

    finction library

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H
+#define _LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,7 +16,8 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
+/*Functions*/
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -27,6 +29,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
 
-#endif
+#endif /* _lists_h */
