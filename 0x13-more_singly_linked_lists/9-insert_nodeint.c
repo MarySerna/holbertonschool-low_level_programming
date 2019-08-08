33fa71f74a3916e9ad60c0eabed4d4d18497f4db
maryserar8903 <mary.serna.8903@gmail.com>
2019-08-08 01:35:49 +0000

    Insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+/**
+* add_nodeint - singly linked list
+* @head: structure
+* @n:value
+* Description: singly linked list node structure
+* Return: a number of elements
+* for Holberton project
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+* insert_nodeint_at_index - program thisgs
+* Result: always return 0
+* @head: pointer to structure
+* @idx: index to add a node
+* @n: value to node
+*
+* Return: value depending on function
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *cp = *head;
+	listint_t *new;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	if (idx == 0)
+	{
+		new = add_nodeint(head, n);
+		return (new);
+	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	while (i < idx - 1)
+	{
+		cp = cp->next;
+		if (cp == NULL)
+			return (NULL);
+		i++;
+	}
+	new->n = n;
+	new->next = cp->next;
+	cp->next = new;
+	return (new);
+}
