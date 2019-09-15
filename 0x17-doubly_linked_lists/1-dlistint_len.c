19e6acc831b4c5849e84ea3fa5d1b6e6efc80601
maryserar8903 <mary.serna.8903@gmail.com>
2019-09-15 22:29:25 +0000

    Liste length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,13 +1,10 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 /**
- * dlistint_len - prints all the elements of the list.
- * @h: pointer
- * 
- *Return: The elements' number of the list
- */
+* dlistint_len - prints all the elements of the list.
+* @h: pointer
+* 
+* Return: The elements' number of the list
+*/
 
 size_t dlistint_len(const dlistint_t *h)
 {
