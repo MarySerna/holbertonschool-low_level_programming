c2aa3058792fd635138e058d905ca1f2bd7bb178
maryserar8903 <mary.serna.8903@gmail.com>
2019-09-16 04:37:56 +0000

    list length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,16 +7,17 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t l = 0;
+
 	if (h == NULL)
-	{
-        return (0);
-	}
+		{
+		return (0);
+		}
 	while (h != NULL)
-	{
-        h = h->next;
-        l++;
-	}
-return (l);
+		{
+		h = h->next;
+		l++;
+		}
+	return (l);
 }
 
 
