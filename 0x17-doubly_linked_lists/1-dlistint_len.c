83412aa0ce49619cad7345f107d5d1d93c9bb0ba
maryserar8903 <mary.serna.8903@gmail.com>
2019-09-16 02:58:07 +0000

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,22 +2,21 @@
 /**
 * dlistint_len - prints all the elements of the list.
 * @h: pointer
-* 
 * Return: The elements' number of the list
 */
-
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t l = 0;
-
 	if (h == NULL)
 	{
-		return (0);
+        return (0);
 	}
 	while (h != NULL)
 	{
-		h = h->next;
-		l++;
+        h = h->next;
+        l++;
 	}
-	return (l);
+return (l);
 }
+
+
