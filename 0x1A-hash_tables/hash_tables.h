8be4cb6683275b36039dc2d68e882f38cf9af37f
maryserar8903 <mary.serna.8903@gmail.com>
2019-10-11 17:42:54 +0000

    Header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef HASH_TABLE
 #define HASH_TABLE
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -11,11 +15,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
-
 
 
 /**
@@ -28,9 +31,10 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
+
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
