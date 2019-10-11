9828db70f149a45f372f561af2d85b37a287d47d
maryserar8903 <mary.serna.8903@gmail.com>
2019-10-11 17:27:06 +0000

    creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+* hash_table - 
+*
+*
+*
+*
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL; 	
+	unsigned int i = 0;
+
+	if (size == 0)
+		return(NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+		return(NULL);
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+		return(NULL);
+
+	for(i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+
+	return(new_table);
+}
