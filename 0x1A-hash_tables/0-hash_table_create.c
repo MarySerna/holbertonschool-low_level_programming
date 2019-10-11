9da94b472fdcf4639f9ca16a546a89358148b41e
maryserar8903 <mary.serna.8903@gmail.com>
2019-10-11 17:38:44 +0000

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,12 @@
 #include "hash_tables.h"
 /**
-* hash_table - 
-*
-*
-*
-*
+* hash_table - creates a hash table 
+* @size: size of array
+* Return : a pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL; 	
+	hash_table_t *new_table = NULL;
 	unsigned int i = 0;
 
 	if (size == 0)
