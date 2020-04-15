373e1ced0643c9e1911d2feb16002a8b3c1e9350
MarySerna <mary.serna.8903@gmail.com>
2020-04-15 20:18:43 +0000

    library

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_A
+#define SEARCH_A
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
+int binary_search(int *array, size_t size, int value);
+
+#endif
